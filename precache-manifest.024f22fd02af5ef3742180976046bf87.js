3618ec5e02e67028e5adbd0262169e7bf7882056
Alexandr <winultra97@mail.ru>
2020-06-20 21:46:10 +0500

    Updates

---
+++ precache-manifest.024f22fd02af5ef3742180976046bf87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ee39c795441430e7794e6dec2f2266",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2fe594f2e1f9675672a",
+    "url": "./static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "ef69e931963263119537",
+    "url": "./static/css/main.5b281bcf.chunk.css"
+  },
+  {
+    "revision": "f2fe594f2e1f9675672a",
+    "url": "./static/js/2.756958e6.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.756958e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef69e931963263119537",
+    "url": "./static/js/main.ea8fbe4f.chunk.js"
+  },
+  {
+    "revision": "a19d6302fdfd1549f5fc",
+    "url": "./static/js/runtime-main.760c15e9.js"
+  }
+]);
